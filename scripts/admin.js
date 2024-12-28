9aac66e3cda6425441a81bc1b285783815614548
gouri22-art <prajaktapk2208@gmail.com>
2024-12-28 16:25:12 +0530

    Books Added successfuly

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,14 +1,14 @@
-const baseUrl="http://localhost:3000/books";
+const baseUrl = "http://localhost:3000/books";
 
 //check if admin is logged in
 const loginData = JSON.parse(localStorage.getItem("loginData"));
-if(!loginData || loginData.email !== "admin@empher.com"){
+if (!loginData || loginData.email !== "admin@empher.com") {
     alert("Admin not Logged in");
-    window.location.href = "index.html";  
+    window.location.href = "index.html";
 }
 
 //Add a new bok
-document.getElementById("addBookForm").addEventListener("submit",async(e)=>{
+document.getElementById("addBookForm").addEventListener("submit", async (e) => {
     e.preventDefault();
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
@@ -17,36 +17,50 @@ document.getElementById("addBookForm").addEventListener("submit",async(e)=>{
         title,
         author,
         category,
-        isAvailable:true,
-        isVerified:true,
-        borrowedDays:null,
-        imageUrl:"https://m.media-amazon.com/images/I/71ZB18P3inL._SY522_.jpg",
+        isAvailable: true,
+        isVerified: true,
+        borrowedDays: null,
+        imageUrl: "https://m.media-amazon.com/images/I/71ZB18P3inL._SY522_.jpg",
     };
-    try{
-        await fetch(baseUrl),{
+    console.log("Submitting new book:", book)
+    try {
+        const response = await fetch(baseUrl, {
             method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body:JSON.stringify(book),
-        }
+            headers: { "Content-Type":"application/json" },
+            body: JSON.stringify(book),
+        });
+        console.log("Response status", response.status);
+        const addedBook = await response.json();
+        console.log("Added book:",addedBook);
+    if (response.ok) {
         alert("Book Added Successfully");
         loadBooks();
-    }catch(error){
-        alert("Failed to add book.Please try again later.");
+    }
+    else {
+        alert("failed to add book")
+    }
+    //     alert("Book Added Successfully");
+    // console.lof("data", book)
+    // loadBooks();
+    }catch (error) {
+    alert("Failed to add book.Please try again later.");
     }
 });
 
 //load all books
-const loadBooks = async()=>{
-    try{
+const loadBooks = async () => {
+    console.log("Loading books...")
+    try {
         const response = await fetch(baseUrl);
         const books = await response.json();
+        console.log("Fetched Books:",books)
 
         const grid = document.getElementById("booksGrid");
         grid.innerHTML = " ";
 
-        books.forEach((book)=>{
+        books.forEach((book) => {
             const card = document.createElement("div");
-            card.className="card";
+            card.className = "card";
 
             card.innerHTML = `
             <img src="${book.imageUrl}" alt="${book.title}">
@@ -58,36 +72,36 @@ const loadBooks = async()=>{
             <button onClick="deleteBook(${book.id})">Delete Book</button>`;
             grid.appendChild(card);
         });
-    }catch(error){
+    } catch (error) {
         alert("Failed to load books.Please try again later.")
     }
 };
 //Verify Book
-const verifyBook = async(id)=>{
-    if(confirm("Are you sure to Verify..?")){
-        try{
-            await fetch(`${baseUrl}/${id}`,{
-                method:"PATCH",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify({isVerified:true}),
+const verifyBook = async (id) => {
+    if (confirm("Are you sure to Verify..?")) {
+        try {
+            await fetch(`${baseUrl}/${id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ isVerified: true }),
             });
             alert("Book Verified Successfully");
             loadBooks();
-        }catch(error){
+        } catch (error) {
             alert("Failed to verify book.Please try again later.")
         }
     }
 };
 
 //Delete a book
-const deleteBook = async(id)=>{
-    if(confirm("Are you sure to Delete..?")){
-        try{
-            await fetch(`${baseUrl}/${id}`,{method:"DELETE"});
+const deleteBook = async (id) => {
+    if (confirm("Are you sure to Delete..?")) {
+        try {
+            await fetch(`${baseUrl}/${id}`, { method: "DELETE" });
             alert("Book Deleted Successfully");
             loadBooks();
 
-        }catch(error){
+        } catch (error) {
             alert("Failed to delete book.Please try again later.");
         }
     }
---
+++ scripts/books.js
@@ -0,0 +1,107 @@
+const baseUrl = "http://localhost:3000/books";
+
+//check if user is logged in
+const loginData = JSON.parse(localStorage.getItem("loginData"));
+if (!loginData || loginData.email !== "user@empher.com") {
+    alert("Admin not Logged in");
+    window.location.href = "index.html";
+}
+
+//load available books
+
+document.getElementById("showAvaliableBooks").addEventListener("click", async () => {
+    try {
+        const response = await fetch(`${baseUrl}?isAvailable=true`);
+        const books = await response.json();
+
+        const grid = document.getElementById("booksGrid");
+        grid.innerHTML = " ";
+
+        books.forEach((book) => {
+            const card = document.createElement("div");
+            card.className = "card";
+
+            card.innerHTML = `
+            <img src="${book.imageUrl}" alt="${book.title}">
+            <h3>${book.title}</h3>
+            <p>Author:${book.author}</p>
+            <p>Category:${book.category}</p>
+            <button onClick="borrowBook(${book.id})">Borrow Book</button>`;
+            grid.appendChild(card);
+
+        });
+    } catch (error) {
+        alert("Failed to load available books.Please try again later.");
+    }
+});
+
+//Borrow book
+
+const borrowBook = async (id) => {
+    const days = prompt("Enter borrowing duration(1-10 days):");
+    if (days && days > 0 && days <= 10) {
+        try {
+            await fetch(`${baseUrl}/${id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ isAvailable: false, borrowBook: days }),
+            });
+            alert("Book Borrowed Successfully");
+            document.getElementById("showAvailableBooks").click();//reload available books
+
+        } catch (error) {
+            alert("Failed to borrow book.Please try again later.");
+        }
+    } else {
+        alert("Invalid Duration.Please enter a value between 1 to 10")
+    }
+
+};
+
+//load borrowed book
+document.getElementById("showBorrowedBooks").addEventListener("click",async()=>{
+    try{
+        const response = await fetch(`${baseUrl}?isAvailable=false`);
+        const books = await response.json();
+
+        const grid = document.getElementById("booksGrid");
+        grid.innerHTML = " ";
+
+        books.forEach((books)=>{
+            const card = document.createElement("div");
+            card.className = "card";
+
+            card.innerHTML = `
+            <img src="${book.imageUrl}" alt="${book.title}">
+            <h3>${book.title}</h3>
+            <p>Author:${book.author}</p>
+            <p>Category:${book.category}</p>
+            <p>Borrowed for:${book.borrowedDays} days</p>
+            <button onClick="returnBook(${book.id})">Return Book</button>`;
+            grid.appendChild(card);
+
+            
+        });
+    }catch(error){
+        alert("Failed to load borrowed books.Please try again later.");
+    }
+});
+
+//return a book 
+const returnBook = async (id)=>{
+    if(confirm("Are you sure to return book ..?")){
+        try{
+            await fetch(`${baseUrl}/${id}`,{
+                method:"PATCH",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify({isAvailable:true,borrowedDays:null}),
+            });
+            alert("Book Returned Successfully.");
+            document.getElementById("showBorrowedBooks").click();// reload borrowed books
+
+        }catch(error){
+            alert("Failed to return book.Please try again later.");
+        }
+
+    }
+}
